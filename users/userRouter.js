f92ffe88a447a2320bbeb107ad7bae458fd629a5
LISA_COOK <lisabpink@gmail.com>
2020-02-12 15:26:59 -0500

    LDC delete user

--- users/userRouter.js
+++ users/userRouter.js
@@ -28,6 +28,24 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   // do your magic!
+  const userId = req.params.id;
+
+  db.getById(userId)
+  .then(specificUser => {
+    if (userId) {
+      res.status(200).json(specificUser);
+    } else {
+      res.status(404).json({
+        message: "The post with the specific ID does not exist"
+      })
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: "The post information could not be retrieved"
+    });
+  });
 });
 
 router.get("/:id/posts", (req, res) => {
@@ -36,6 +54,24 @@ router.get("/:id/posts", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // do your magic!
+  const delId = req.params.id;
+
+  db.remove(delId)
+  .then(deletedUser => {
+    if (!delId) {
+      res.status(404).json({
+        message: "The post with the specific ID does not exist"
+      }) 
+    } else {
+      res.status(200).json({ deletedUser });
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: "The post could not be removed"
+    });
+  });
 });
 
 router.put("/:id", (req, res) => {
