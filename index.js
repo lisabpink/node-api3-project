b74f0621c218422d457a7359f392675f1edc766a
LISA_COOK <lisabpink@gmail.com>
2020-02-13 11:38:18 -0500

    LDC made port dynamic

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 // code away!
+require('dotenv').config(); 
 const server = require("./server.js");
 
-server.listen(7000, () => {
-    console.log("server running on http://localhost:7000")
+// server.listen(7000, () => {
+//     console.log("server running on http://localhost:7000")
+// });
+
+
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log("server running on http://localhost:${port}")
 });
+
+
